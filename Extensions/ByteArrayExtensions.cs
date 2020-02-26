7bd484d185d3b1b1d796d03a28f0194ea621cc3b
Ilya Irbitskiy <CastlingCheckmate97@gmail.com>
2020-02-27 02:27:39 +0300

    Added Extensions library

---
+++ Extensions/ByteArrayExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Extensions.ByteArrayExtensions
+{
+
+    public static class ByteArrayExtensions
+    {
+
+        public static string ToHexadecimalString(this byte[] bytes)
+        {
+            return string.Join(Environment.NewLine, BitConverter.ToString(bytes).Replace("-", string.Empty));
+        }
+
+    }
+
+}
---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Extensions.StringExtensions
+{
+
+    public static class StringExtensions
+    {
+
+        public static IEnumerable<string> SplitByChunks(this string stringToSplit, int chunkMaxSize)
+        {
+            for (int i = 0; i < stringToSplit.Length; i += chunkMaxSize)
+            {
+                yield return stringToSplit.Substring(i, Math.Min(chunkMaxSize, stringToSplit.Length - i));
+            }
+        }
+
+    }
+
+}
